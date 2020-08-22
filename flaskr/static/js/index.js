9c350c585ce3c7127e3749c15580988c11fb0509
Ratana <ratanawang8@gmail.com>
2020-08-22 11:47:52 -0700

    add second scroll down arrow

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -37,11 +37,10 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-// scroll down arrow
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
-
+// // scroll down arrow
+// $(function() {
+//   $('a[href*=#]').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//   });
+// });
