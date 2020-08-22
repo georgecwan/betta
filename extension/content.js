5503eb684cb23fa0fb70293155565e443397d40f
Brian Zhang <bbrianzzhang@gmail.com>
2020-08-22 10:22:38 -0700

    add snackbar

--- extension/content.js
+++ extension/content.js
@@ -26,13 +26,4 @@ chrome.runtime.onMessage.addListener(function(request) {
             alert(result.blacklist.join("\n"))
         })
     }
-    else if(request.message == "clearBlacklist"){
-        chrome.storage.sync.get(['blacklist'], function(result){
-            const list = [];
-            chrome.storage.sync.set({'blacklist': list})
-            var snackbar = document.getElementById('snackbarbetta')
-            snackbar.className = 'show'
-            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000)
-        })
-    }
 })
--- extension/popup.js
+++ extension/popup.js
@@ -36,8 +36,9 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.reset').addEventListener('click', onclick, false)
 
     function onclick () {
-        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {message: "clearBlacklist"})
+        chrome.storage.sync.get(['blacklist'], function(result){
+            const list = [];
+            chrome.storage.sync.set({'blacklist': list})
         })
         var x = document.getElementById("snackbar")
         x.className = "show"
