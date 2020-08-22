121b9c9939df2954a5e49074fddbabd06e6f1934
Nanxin Qin <nancinqin@hotmail.com>
2020-08-22 02:41:15 -0700

    about page, arrow scroll position need fix, ignore all picture links

--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -8,7 +8,13 @@ from flaskr.db import get_db
 
 bp = Blueprint("dashboard", __name__)
 
+
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route("/about")
+def about():
+    return render_template("about.html")
 
