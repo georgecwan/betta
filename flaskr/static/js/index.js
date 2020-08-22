d04b3b68b5443c1cb9798289cb4bc83d056cf519
Ratana <ratanawang8@gmail.com>
2020-08-22 08:55:49 -0700

    hide navbar on scroll, add parallax scrolling effect

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -24,3 +24,15 @@ function clickHamburger() {
   }
   console.log("hamburger has been clicked");
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+}
