1117261845434e54f56e51c7f26606de5f171fe2
georgecwan <georgecwan@users.noreply.github.com>
2020-08-22 13:58:33 -0700

    Remove unnecessary alerts

--- extension/content.js
+++ extension/content.js
@@ -14,25 +14,18 @@ chrome.runtime.onMessage.addListener(function(request) {
                 newList.push(webPage)
                 chrome.storage.sync.set({'blacklist': newList}, function () {
                     alert(webPage + " added to blacklist.")
-                    alert("Blacklist:\n" + newList.join("\n"))
                 })
             }
             else {
-                for(var i=0; i<newList.length; i++){
+                for(var i=0; i<newList.length; i++) {
                     if(newList[i] == webPage){
                         newList.splice(i, 1)
                     }
                 }
                 chrome.storage.sync.set({'blacklist': newList}, function () {
                     alert(webPage + " removed from blacklist.")
-                    alert("Blacklist:\n" + newList.join("\n"))
                 })
             }
-        })
-    }
-    else if(request.message == "showBlacklist"){
-        chrome.storage.sync.get(['blacklist'], function(result){
-            alert(result.blacklist.join("\n"))
         })
     }
 })
