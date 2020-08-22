e2ac3f9e54159563d2198437c0899fb93dd6e772
Ratana <ratanawang8@gmail.com>
2020-08-22 10:40:06 -0700

    add home page title and scroll down arrow

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -36,3 +36,12 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// scroll down arrow
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
