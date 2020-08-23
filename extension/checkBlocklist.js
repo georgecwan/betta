1beb39daac73b02499ae1a51ee4732de7c34c5dc
Brian Zhang <bbrianzzhang@gmail.com>
2020-08-23 02:55:47 -0700

    add consequence for dead fish

--- extension/checkBlocklist.js
+++ extension/checkBlocklist.js
@@ -4,8 +4,17 @@ function allowOrBan() {
     chrome.storage.sync.get(['blacklist'], function(result){
         let bannedurl = result.blacklist.includes(window.location.origin);
         if(bannedurl){
-            alert("Blocklisted Website! Bad!");
-            alert("Your blocklisted websites are:\n" + result.blacklist.join("\n"));
+            chrome.storage.sync.get(['fishHealth'], function(health){
+                if(health.fishHealth <= 0){
+                    while(true){
+                        alert("Your fish is dead! Please close this website and visit non-blocklisted websites to regain health points.")
+                    }
+                }
+                else{
+                    alert("Blocklisted Website! Bad!");
+                    alert("Your blocklisted websites are:\n" + result.blacklist.join("\n"));
+                }
+            })
             chrome.runtime.sendMessage({message: "blocklisted"});
             toAdd = -20
         }
@@ -19,12 +28,15 @@ function allowOrBan() {
             }
             else if(health.fishHealth <= 20 && toAdd < 0){
                 newHealth = 0;
+                while(true){
+                    alert("Your fish is dead! Please close this website and visit non-blocklisted websites to regain health points.")
+                }
             }
             else{
                 newHealth = health.fishHealth + toAdd;
             }
             chrome.storage.sync.set({'fishHealth': newHealth}, function () {
-                alert("New health: " + String(newHealth));
+                //alert("New health: " + String(newHealth));
             })
         })
     })
